72207819b5a1ad608c6aedf1faa00dcaa6d8505f
Damian Socha <damiansoch.ds@gmail.com>
2022-06-27 07:42:34 +0100

    basic animations

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
-$(function () {
-  // Fade out element over 2000ms
-  $(".red-box").fadeOut(2000);
+$(function(){
+  $(".red-box").fadeTo(2000, 0.2);
+  $(".green-box").fadeTo(1000, 0);
+  $(".blue-box").fadeTo(700, 0.8);
 
-  // Fade back in over 1000ms
-  $(".red-box").fadeIn(1000);
+  $(".blue-box").fadeToggle(1000);
+  $(".blue-box").fadeToggle(1000);//fades back in only to 0.8 - the same it was before the toggle
 
-  // Fade element to specific opacity (here 50%)
-  $(".blue-box").fadeTo(1000, 0.5);
-
-  // Note that the blue box animation starts right away, whereas the second
-  // red box animation waits until the first animation finished.
-  // This is because each element has its own animation queue which is
-  // executed one after the other. So while the red box is still "busy" with
-  // its first animation, the second animation waits in the queue.
-
-  // Fade element in or out, depending on current status
-  $(".blue-box").fadeToggle();
-  $(".blue-box").fadeToggle();
-
-  // Notice that fadeToggle fades back to the blue box's previous opacity,
-  // thus only back to 50% opacity.
-});
+  $(".red-box1").hide(1000);
+  setTimeout(function(){
+    $(".red-box1").show(1000);
+  },2000)
+  // we can call funciton toggle() if we are not sure if the element is shown or hiden
+$(".green-box1").slideUp(2000);
+$(".green-box1").slideDown(2000);
+$("p").hide();
+$("p").show(2000);
+$(".blue-box1").slideToggle(2000)
+}) 
